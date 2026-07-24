bc9364d52b1c6d8e49d6f717afd245b78892803c
Raviraj <ravirajaade299@gmail.com>
2026-07-24 21:17:02 +0530

    Code Update

---
+++ Datatype_String_X.js
@@ -0,0 +1,5 @@
+let userName = "Raviraj"
+let userCity = "Pune"
+
+console.log("Hello, "+ userName)
+console.log(userCity)
---
+++ Datatype_String_XX.js
@@ -0,0 +1,8 @@
+let userName = "Raviraj"
+let userCity = "Pune"
+
+let userCourse = `Javascript`
+
+console.log("Hello, "+ userName)
+console.log(userCity)
+console.log(userCourse)
