3052c8a6d5c1fea58f775af818b51aa51b00505b
Raviraj <ravirajaade299@gmail.com>
2026-07-25 21:12:54 +0530

    Code Update

--- Datatype_Number.js
+++ Datatype_Number.js
@@ -4,3 +4,11 @@ let expNr =  1.4e15 //exponentials
 let octNr = 0o10; //decimal version would be 8
 let hexNr = 0x3E8; //decimal version would be 1000
 let binNr = 0b101; //decimal version would be 5
+
+console.log("Int Number " + intNr)
+console.log("Decimal Number " + decNr)
+console.log("exponentials Number " + expNr)
+console.log("Octal Number " + octNr)
+console.log("Hexadecimal Number " + hexNr)
+console.log("Binary Number " + binNr)
+
