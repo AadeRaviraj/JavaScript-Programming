a4d4db967091a81d0ec7348569c745d3901edd38
Raviraj <ravirajaade299@gmail.com>
2026-07-25 21:28:57 +0530

    Code Update

---
+++ Datatype_Boolean.js
@@ -0,0 +1,6 @@
+let boolTrue = true;
+
+console.log("True Value : "+ boolTrue);
+
+let boolFalse = false;
+console.log("False Value : "+boolFalse);
---
+++ Datatype_BooleanX.js
@@ -0,0 +1,6 @@
+let objectIsDeleted = true;
+
+console.log("True Value : "+ objectIsDeleted);
+
+let lightIson = false;
+console.log("False Value : "+lightIson);
