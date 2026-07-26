2b16bd162825072fe6e9148f2d714d18e3410b8e
Raviraj <ravirajaade299@gmail.com>
2026-07-26 19:49:13 +0530

    Code Update

---
+++ Datatype_ConvertingX.js
@@ -0,0 +1,4 @@
+let s1 = "2";
+let s2 = "4";
+
+console.log( s1 + s2 ); // 8
---
+++ Datatype_ConvertingXX.js
@@ -0,0 +1,7 @@
+let strToNr = "2";
+strToNr = Number(strToNr)
+console.log("Convert String to number : "+strToNr, "Type of variable : "+ typeof(strToNr))
+
+let nrToStr = 2;
+nrToStr = String(nrToStr);
+console.log("Convert Number to string : "+ nrToStr, "Type of variable : "+ typeof(nrToStr))
