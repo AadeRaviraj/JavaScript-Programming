b8df9215723375b650e26fbe248fad105a38ecc5
Raviraj <ravirajaade299@gmail.com>
2026-07-26 19:57:39 +0530

    Code Update

---
+++ Datatype_ConvertingXXX.js
@@ -0,0 +1,15 @@
+let str = "6";
+let num = 2;
+
+console.log(Number(str) + num) // 8
+
+let str1 = "6";
+let num1 = 2;
+
+console.log(str1 + num1) // 62
+
+
+let str2 = "6";
+let num2 = "2";
+
+console.log(Number(str2) + num2) // 62
