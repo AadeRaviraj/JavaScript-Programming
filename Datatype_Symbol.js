39d148142eb3db5b60bffe9277cd5fd34322ad72
Raviraj <ravirajaade299@gmail.com>
2026-07-26 19:28:21 +0530

    Code Update

--- Datatype_Symbol.js
+++ Datatype_Symbol.js
@@ -1,5 +1,5 @@
 let s1 = "Raviraj";
 let s2 = "Raviraj";
 
-console.log("Comparison of two variable : "+  s1 === s2);
-console.log( s1 === s2);
+console.log("Comparison of two variable : "+  s1 === s2); // false 
+console.log( s1 === s2); // true
