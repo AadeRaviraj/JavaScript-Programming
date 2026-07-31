30d9682a325a00e549a09d88ff1e6a3ee30ea19e
Raviraj <ravirajaade299@gmail.com>
2026-07-31 21:23:36 +0530

    Code Update

--- Operator_Unary.js
+++ Operator_Unary.js
@@ -1,8 +1,12 @@
 let nr1 = 5 
-nr1++
+let n = 4
+let  k = 3 
+
+console.log(nr1++ + ++n * k++)
 console.log("Post Increment value :", nr1)
 
 let nr2 = 4 
 nr2--
 console.log("Post decrement value : "+ nr2)
 
+ 
---
+++ Operator_Unary_X.js
@@ -0,0 +1,4 @@
+let nr1 = 4;
+let nr2 = 5;
+let nr3 = 2;
+console.log(nr1++ + ++nr2 * nr3++);   // 4  
