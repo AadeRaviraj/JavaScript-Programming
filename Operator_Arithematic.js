91ad19a66835c5b318cd0cc9cc32d3a1773e35b7
Raviraj <ravirajaade299@gmail.com>
2026-07-31 20:55:26 +0530

    Code Update

---
+++ Operator_Arithematic.js
@@ -0,0 +1,9 @@
+let num1 = 20 ;
+let num2 = 3 ;
+
+console.log("Addition of numbers : "+ (num1 + num2));
+console.log("Subtraction of numbers : "+ (num1 - num2));
+console.log("Multiplication of numbers : "+ (num1 * num2));
+console.log("Division of numbers : "+ (num1 / num2));
+console.log("Exponentiation of numbers : "+ (num1 ** num2));
+console.log("Modulus of numbers : "+ (num1 % num2));
